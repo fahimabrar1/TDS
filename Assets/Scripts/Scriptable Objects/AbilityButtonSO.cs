80f4daf88a547ce6e8d4bf2968fefa6eb6bd57eb
Fahim Abrar <fahim.abrar101@gmail.com>
2024-09-07 02:19:19 +0600

    Created  Data Type Scripts for Ability Buttons

---
+++ Assets/Scripts/Scriptable Objects/AbilityButtonSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AbilityButtonSO", menuName = "TDS/Ability Buttons Data/AbilityButtonSO", order = 0)]
+public class AbilityButtonSO : ScriptableObject
+{
+    public int cost;
+    public Sprite sprite;
+    public AbilityButton.AbilityButtonType abilityButtonType;
+}

a046b805d3cd807146b75436c103f4451ed1de8d
Fahim Abrar <fahim.abrar101@gmail.com>
2024-09-09 20:12:33 +0600

    Update prefabs and scene

--- Assets/Scripts/Player/IDamagable.cs
+++ Assets/Scripts/Player/IDamagable.cs
@@ -2,3 +2,7 @@ public interface IDamagable
 {
     void OnTakeDamage(int damage);  // Method to handle taking damage
 }
+
+
+public interface IPlayerDamagable : IDamagable { }
+public interface IEnemyDamagable : IDamagable { }

e2daa4ef1dffff2d21a1aec816a91d755c3e8623
Fahim Abrar <fahim.abrar101@gmail.com>
2024-09-12 17:43:29 +0600

    Updated Data classes

--- Assets/Scripts/Upgrade/EnergyData.cs
+++ Assets/Scripts/Upgrade/EnergyData.cs
@@ -1,49 +1,28 @@
 [System.Serializable]
-public class EnergyGenerateData
+public class EnergyGenerateData : BaseUpgradeData
 {
-    public float defaulEnergyRate = 0.2f;    // The default health value
-    public float energyRateIncrement = .02f;  // The amount by which health increases on upgrade
-    public int defaultEnergyRateCost = 50;       // The cost of upgrading the health
-    public int energyRateCostIncrement = 50;       // The cost of upgrading the health
 
     // Constructor to initialize HealthData with default values
     public EnergyGenerateData()
     {
+        ResetData();
     }
 
 
     // Constructor to initialize HealthData with default values
-    public EnergyGenerateData(float defaulEnergyRate, int defaultEnergyRateCost)
+    public EnergyGenerateData(float defaultValue, int defaultCost)
     {
-
-        this.defaulEnergyRate = defaulEnergyRate;
-        this.defaultEnergyRateCost = defaultEnergyRateCost;
-    }
-
-    // Method to apply an upgrade, increases health by healthIncrement
-    public void ApplyUpgrade()
-    {
-        defaulEnergyRate += energyRateIncrement;
-        defaultEnergyRateCost += energyRateCostIncrement;
-    }
-
-    // Method to get the current health
-    public float GetEnergyRate()
-    {
-        return defaulEnergyRate;
+        this.defaultValue = defaultValue;
+        this.defaultCost = defaultCost;
     }
 
     // Method to get the cost of upgrading the health
-    public int GetUpgradeCost()
+    public override void ResetData()
     {
-        return defaultEnergyRateCost;
-    }
+        defaultValue = 0.2f;
+        valueIncrementBy = 0.02f;
+        defaultCost = 50;
+        costIncrementBy = 50;
 
-
-    // Method to get the cost of upgrading the health
-    public void ResetData()
-    {
-        defaulEnergyRate = 0.2f;    // The default health value
-        defaultEnergyRateCost = 50;       // The cost of upgrading the health
     }
 }
--- Assets/Scripts/Upgrade/HealthData.cs
+++ Assets/Scripts/Upgrade/HealthData.cs
@@ -1,49 +1,32 @@
 [System.Serializable]
-public class HealthData
+public class HealthData : BaseUpgradeData
 {
-    public int defaultHealth = 500;    // The default health value
-    public int healthIncrement = 100;  // The amount by which health increases on upgrade
-    public int defaulthealthCost = 150;       // The cost of upgrading the health
-    public int healthCostIncrement = 50;       // The cost of upgrading the health
 
+    // public UpgradeCostModel upgradeCostModel;
     // Constructor to initialize HealthData with default values
     public HealthData()
     {
+        ResetData();
     }
 
 
     // Constructor to initialize HealthData with default values
-    public HealthData(int defaultHealth, int defaulthealthCost)
+    public HealthData(int defaultValue, int defaultCost)
     {
 
-        this.defaultHealth = defaultHealth;
-        this.defaulthealthCost = defaulthealthCost;
-    }
-
-    // Method to apply an upgrade, increases health by healthIncrement
-    public void ApplyUpgrade()
-    {
-        defaultHealth += healthIncrement;
-        defaulthealthCost += healthCostIncrement;
-    }
-
-    // Method to get the current health
-    public int GetHealth()
-    {
-        return defaultHealth;
-    }
-
-    // Method to get the cost of upgrading the health
-    public int GetUpgradeCost()
-    {
-        return defaulthealthCost;
+        this.defaultValue = defaultValue;
+        this.defaultCost = defaultCost;
     }
 
 
     // Method to get the cost of upgrading the health
-    public void ResetData()
+    public override void ResetData()
     {
-        defaultHealth = 500;    // The default health value
-        defaulthealthCost = 150;       // The cost of upgrading the health
+        defaultValue = 500;
+        valueIncrementBy = 100;
+        defaultCost = 150;
+        costIncrementBy = 50;
+
     }
+
 }

f6904e2b920ca78752e1b52a2cc5bc72bacf88c5
Fahim Abrar <fahim.abrar101@gmail.com>
2024-09-12 15:41:49 +0600

    update with health data

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,18 +8,48 @@ public class GameManager : MonoBehaviour
     [Tooltip("The singleton instance of the game manager.")]
     public static GameManager instance;
 
+
+    public HealthData healthData;
+    public FileProcessorr<HealthData> healthDataProcessor = new();
+
+
+
+
     void Awake()
     {
+
         if (instance != null)
         {
-            instance = this;
-            DontDestroyOnLoad(this);
+            Destroy(this.gameObject);
         }
         else
         {
-            Destroy(this);
+            instance = this;
         }
 
         DOTween.Init();
+        healthData = new();
+        LoadData();
+    }
+
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    async void LoadData()
+    {
+        await healthDataProcessor.OnLoadAsync(healthData);
+    }
+
+
+
+
+    /// <summary>
+    /// This function is called when the MonoBehaviour will be destroyed.
+    /// </summary>
+    void OnDestroy()
+    {
+        healthDataProcessor.OnSaveAsync(healthData);
     }
 }

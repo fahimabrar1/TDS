9ce1a07522b3ce21481c76832dacc87a789a35b4
Fahim Abrar <fahim.abrar101@gmail.com>
2024-09-12 23:54:56 +0600

    Added Action To Update Values observing

--- Assets/Scripts/Upgrade/model/BaseUpgradeData.cs
+++ Assets/Scripts/Upgrade/model/BaseUpgradeData.cs
@@ -1,20 +1,37 @@
+using System;
 using UnityEngine;
 
 [System.Serializable]
 public class BaseUpgradeData : IApplyUpgradable
 {
-
-    public float defaultValue = 0;
+    [SerializeField]
+    private float defaultValue = 0;
     public float valueIncrementBy = 0;
     public int defaultCost = 0;
     public int costIncrementBy = 0;
-
     public int level = 1;
+    public int levelIncrementBy = 0;
+
+    public Action OnUpdateDDefaultValue;
+
+
+
+    // Property to safely manipulate coins
+    public float DefaultValue
+    {
+        get => defaultValue;
+        protected set
+        {
+            defaultValue = value;
+            OnUpdateDDefaultValue?.Invoke();  // Notify listeners when coins change
+        }
+    }
+
 
     public void ApplyUpgrade()
     {
         MyDebug.Log($"Apply Upgrade {nameof(BaseUpgradeData)}");
-        defaultValue += valueIncrementBy;
+        DefaultValue += valueIncrementBy;
         defaultCost += costIncrementBy;
         level++;
     }
@@ -22,13 +39,13 @@ public class BaseUpgradeData : IApplyUpgradable
 
     public float GetValue()
     {
-        return Mathf.Round(defaultValue * 100f) / 100f;
+        return Mathf.Round(DefaultValue * 100f) / 100f;
     }
 
 
     public float GetValueRounded()
     {
-        return Mathf.RoundToInt(defaultValue);
+        return Mathf.RoundToInt(DefaultValue);
     }
 
     public int GetUpgradeCost()

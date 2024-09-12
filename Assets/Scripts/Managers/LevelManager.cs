300da1b0f289bc3fe7d0777a45e03f719533cb2d
Fahim Abrar <fahim.abrar101@gmail.com>
2024-09-12 23:15:40 +0600

    Spawn Player On Scene

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -6,6 +6,10 @@ public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
 
+    [Tooltip("The player gameobject")]
+    public GameObject plaeyrObj;
+    public Transform playerSpawnPoint;
+
     [Tooltip("The player in the level.")]
     public Transform playerTransform;
 
@@ -25,11 +29,24 @@ public class LevelManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if (GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            SpawnPlayer();
+        }
     }
 
 
     public void RemoveCrate(Transform transform)
     {
         crates.Remove(transform);
+    }
+
+
+
+    public void SpawnPlayer()
+    {
+        var obj = Instantiate(plaeyrObj, playerSpawnPoint.position, Quaternion.identity, playerSpawnPoint);
+        playerTransform = obj.transform;
     }
 }

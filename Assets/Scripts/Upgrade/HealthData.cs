e970b06dd13c332cab8f7f98cbcfdb9cd9a42942
Fahim Abrar <fahim.abrar101@gmail.com>
2024-09-12 15:09:59 +0600

    Create HealthData

---
+++ Assets/Scripts/Upgrade/HealthData.cs
@@ -0,0 +1,35 @@
+[System.Serializable]
+public class HealthData
+{
+    public int defaultHealth = 500;    // The default health value
+    public int healthIncrement = 100;  // The amount by which health increases on upgrade
+    public int defaulthealthCost = 150;       // The cost of upgrading the health
+    public int healthCostIncrement = 50;       // The cost of upgrading the health
+
+    // Constructor to initialize HealthData with default values
+    public HealthData(int defaultHealth, int defaulthealthCost)
+    {
+
+        this.defaultHealth = defaultHealth;
+        this.defaulthealthCost = defaulthealthCost;
+    }
+
+    // Method to apply an upgrade, increases health by healthIncrement
+    public void ApplyUpgrade()
+    {
+        defaultHealth += healthIncrement;
+        defaulthealthCost += healthCostIncrement;
+    }
+
+    // Method to get the current health
+    public int GetHealth()
+    {
+        return defaultHealth;
+    }
+
+    // Method to get the cost of upgrading the health
+    public int GetUpgradeCost()
+    {
+        return defaulthealthCost;
+    }
+}

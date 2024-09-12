ff1059b013aa083bbf44b856cb6d0fe237c585f7
Fahim Abrar <fahim.abrar101@gmail.com>
2024-09-12 15:42:16 +0600

    partially connected health data with UI buttom

--- Assets/Scripts/UI/LevelMenuUI.cs
+++ Assets/Scripts/UI/LevelMenuUI.cs
@@ -1,18 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Button healthUpgradeButton; // Example button for upgrading
+    public Button energyUpgradeButton; // Example button for upgrading
+
+
+
+    private void OnEnable()
     {
-        
+        // Subscribe to currency change events
+        if (CurrencyManager.Instance != null)
+        {
+            CurrencyManager.Instance.OnCurrencyChanged += UpdateUpgradeButton;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        // Unsubscribe to prevent memory leaks
+        if (CurrencyManager.Instance != null)
+        {
+            CurrencyManager.Instance.OnCurrencyChanged -= UpdateUpgradeButton;
+        }
     }
+
+
+
+
+
+    // This method is called whenever coins change
+    private void UpdateUpgradeButton()
+    {
+        if (CurrencyManager.Instance != null && GameManager.instance.healthData != null)
+        {
+
+            // Enable the button if enough coins, otherwise disable
+            healthUpgradeButton.interactable = CurrencyManager.Instance.Coins >= GameManager.instance.healthData.defaulthealthCost;
+        }
+    }
+
+    // Upgrade method, called when the button is pressed
+    public void OnUpgradeButtonPressed()
+    {
+        if (CurrencyManager.Instance != null && GameManager.instance.healthData != null && CurrencyManager.Instance.Coins >= GameManager.instance.healthData.defaulthealthCost)
+        {
+            CurrencyManager.Instance.SpendCoins(GameManager.instance.healthData.defaulthealthCost);
+            MyDebug.Log("Upgrade successful.");
+        }
+        else
+        {
+            MyDebug.Log("Not enough coins for upgrade.");
+        }
+    }
+
+
 }

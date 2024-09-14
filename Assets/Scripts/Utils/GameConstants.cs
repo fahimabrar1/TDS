896c0393e383e74b62f4fbc9bc891d7721e4073d
Fahim Abrar <fahim.abrar101@gmail.com>
2024-09-14 13:36:05 +0600

    Update GameConstants.cs

--- Assets/Scripts/Utils/GameConstants.cs
+++ Assets/Scripts/Utils/GameConstants.cs
@@ -2,5 +2,7 @@ public class GameConstants
 {
     public const string COIN = "Coins";
     public const string SAVE_FILE_EXTENSION = ".json";
+    public const string SoundOnName = "soundOn";
+    public const string MusicOnName = "musicOn";
 
 }
